e5e9e87e57e2bb151e85541a5c2b22833ca265ce
thazinnaing <thazinniangmm@gmail.com>
2024-02-07 12:00:55 +0630

    feat: adding custom layout styles

---
+++ src/app/products/[productId]/reviews/layout.tsx
@@ -0,0 +1,15 @@
+export default function DetailLayout({
+    children,
+  }: {
+    children: React.ReactNode;
+  }) {
+    return (
+      
+          <div>
+              {children}
+              <h1>Additional features</h1>
+          </div>
+      
+    );
+  }
+  
