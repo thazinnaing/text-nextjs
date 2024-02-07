dcfc632cadd5ad3bb0c1da6b490535628bbb2ff9
thazinnaing <thazinniangmm@gmail.com>
2024-02-07 16:51:18 +0630

    feat: Link and replace

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,11 @@
-
+import Link from "next/link";
 export default function Home() {
   return (
     <div>
       <h1 className="text-blue-400 font-bold">Hello</h1>
       <h2>world</h2>
+      <Link href="/about">About</Link>
+      <Link href="/products">Products</Link>
     </div>
   );
 }
--- src/app/products/page.tsx
+++ src/app/products/page.tsx
@@ -1,8 +1,11 @@
+import Link from "next/link"
 const Products = () => {
     return (
-      <div>
-          This is a product page.
-      </div>
+     <div>
+      <Link href="/products/1">Product 1</Link>
+      <Link href="/products/2" replace>Product 2</Link>
+      <Link href="/products/3">Product 3</Link>
+     </div>
     )
   }
   
