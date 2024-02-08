123117ccf1b2afb58057aa5c08bcbc1db37a9ddf
thazinnaing <thazinniangmm@gmail.com>
2024-02-08 15:05:56 +0630

    feat: sub-navigation in parallel routes

---
+++ src/app/complex-routes/@notifications/archieved/page.tsx
@@ -0,0 +1,15 @@
+import Card from "@/components/card"
+import Link from "next/link"
+
+
+const archieved = () => {
+  return (
+    <Card className="flex flex-col">
+      <div>Archieved page</div>
+      <Link href="/complex-routes">Default</Link>
+    </Card>
+  )
+}
+
+export default archieved
+
--- src/app/complex-routes/@notifications/page.tsx
+++ src/app/complex-routes/@notifications/page.tsx
@@ -1,9 +1,13 @@
 import Card from "@/components/card"
+import Link from "next/link"
 
 const Notifications = () => {
   return (
-    <Card className="">
-      Notifications
+    <Card className="flex flex-col">
+        <div>
+            This is Notifications
+        </div>
+        <Link href="/complex-routes/archieved">archieved</Link>
     </Card>
   )
 }
